eb7f1b437e326d0802d83827ac961fdab72a8bcd
syntax2 <akadian135@gmail.com>
2024-06-22 23:42:23 +0530

    Added the files for different pages

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -26,13 +26,20 @@ const Links = [
     },
 ];
 const Nav = () => {
+    const pathname = usePathname();
+    console.log(pathname);
     return (<nav className="flex gap-8">
-        {Links.map((link, index) =>
-        {
-            return <Link href={link.path} key={index} className={``}></Link>
+        {Links.map((link, index) =>{
+            return ( 
+                <Link href={link.path} key={index} className={`${
+                    link.path === pathname && "text-accent border-b-2 border-accent"
+                    } capitalize font-medium hover:text-accent transition-all`}>
+                    {link.name}
+                </Link>
+            );
         })}
     </nav>
     );
-}
+};
 
-export default Nav
+export default Nav;
