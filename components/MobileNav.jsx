e11c465aed1c8d3de69ffadfdfdb37fda6630222
syntax2 <akadian135@gmail.com>
2024-07-18 18:51:12 +0530

    setup the mobile view and make changes in the button that were created using shadcn .

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -35,7 +35,29 @@ const MobileNav = () => {
             <CiMenuFries className=" text-[32px] text-accent" />
         </SheetTrigger>
         <SheetContent className="flex flex-col">
-            <div>logo </div>
+            {/*logo */}
+            <div className="mt-32 mb-40 text-center text-2xl">
+                <Link href="/">
+                    <h1 className="text-4xl font-semibold">
+                        Ashish<span className="text-accent">.</span>
+                    </h1>
+                </Link>
+            </div>
+            {/*nav */}
+            <nav className="flex flex-col justify-center items-center gap-8">
+                {links.map((link, index) => {
+                    return (
+                        <Link href={link.path} key={index} className={`${
+                            link.path === pathname &&
+                            "text-accent border-b-2 border-accent"
+                            }text-xl capitalize hover:text-accent transition-all`}
+                        >
+                            {link.name}
+                        </Link>
+                    );
+                })}
+            </nav>
+
         </SheetContent>
 
     </Sheet>
