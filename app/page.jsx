a222d032ac45a0e1cb06f2349c69b6b7304aeb17
syntax2 <akadian135@gmail.com>
2024-07-20 10:21:47 +0530

    changed the global css file for heading and make changes in the landing page to add cv and photo and intro section , also created a new component named Social

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,45 @@
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload, fiDownload } from "react-icons/fi";
+
+const Home = () => {
   return (
-    <main>homepage</main>
-  );
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between
+        xl:pt-8 xl:pb-24">
+          { /* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Ashish Kadian</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences for developers and I am proficient in variour programming languages
+              and technologies.
+            </p>
+            { /* button and socials */}
+            <div>
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                {/* <Socials /> */}
+              </div>
+            </div>
+          </div>
+          { /* image */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  )
+
 }
+
+export default Home;
