d674a1fb3607958b68b793ead9de38e435a40a8b
Ashish Kadian <akadian135@gmail.com>
2024-10-23 13:27:23 +0530

    work page of portfolio

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,72 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    discription: "lorem24",
+    stack: [{ name: "Html 5" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+];
+
 const Work = () => {
-    return <div>Work page </div>;
+  const [project, setProject] = useState(projects[0]);
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+              {/* outline num */}
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              {/* project category */}
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              {/* project description */}
+              <p className="text-white/60">{project.description}</p>
+              {/* stack */}
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">Slider</div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Work;
